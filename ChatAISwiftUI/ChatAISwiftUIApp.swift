3efbad3080cdb760b6a9e2aca473ac45287e856a
Yunsu Bae <yunsu@comcom.ai>
2023-07-21 16:47:34 +0900

    Initial Commit

---
+++ ChatAISwiftUI/ChatAISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAISwiftUIApp.swift
+//  ChatAISwiftUI
+//
+//  Created by Comcom on 2023/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ChatAISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
